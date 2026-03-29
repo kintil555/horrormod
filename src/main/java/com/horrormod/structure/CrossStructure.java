86e7a3614549d9186202bde2c18ab2658e54a98f
kintil555 <dzikriganteng52637@gmail.com>
2026-03-29 16:52:55 +0700

    Update CrossStructure.java

--- src/main/java/com/horrormod/structure/CrossStructure.java
+++ src/main/java/com/horrormod/structure/CrossStructure.java
@@ -6,36 +6,36 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
 /**
- * Salib (cross) yang terbuat dari dark oak wood.
- * Bentuk:
- *    [O]       y+4  (top)
- *    [O]       y+3
- * [O][O][O]    y+2  (horizontal arm)
- *    [O]       y+1
- *    [O]       y+0  (base)
+ * Salib (cross) dari cobblestone, berdiri vertikal.
+ * Bentuk (tampak depan):
+ *
+ *    [X]       y+5  (top)
+ *    [X]       y+4
+ * [X][X][X]    y+3  (horizontal arm, 2 blok ke kiri & kanan)
+ *    [X]       y+2
+ *    [X]       y+1
+ *    [X]       y+0  (base di tanah)
  */
 public class CrossStructure {
 
-    private static final Block CROSS_BLOCK = Blocks.DARK_OAK_LOG;
+    private static final Block CROSS_BLOCK = Blocks.COBBLESTONE;
 
     public static void spawn(ServerWorld world, BlockPos base) {
-        // Tiang vertikal (y=0 sampai y=4)
-        for (int i = 0; i <= 4; i++) {
+        // Tiang vertikal (y=0 sampai y=5)
+        for (int i = 0; i <= 5; i++) {
             world.setBlockState(base.up(i), CROSS_BLOCK.getDefaultState());
         }
-        // Palang horizontal di y+2 (kiri dan kanan)
-        world.setBlockState(base.up(2).west(), CROSS_BLOCK.getDefaultState());
-        world.setBlockState(base.up(2).east(), CROSS_BLOCK.getDefaultState());
+        // Palang horizontal di y+3 (2 blok ke kiri dan kanan)
+        world.setBlockState(base.up(3).west(),        CROSS_BLOCK.getDefaultState());
+        world.setBlockState(base.up(3).west().west(), CROSS_BLOCK.getDefaultState());
+        world.setBlockState(base.up(3).east(),        CROSS_BLOCK.getDefaultState());
+        world.setBlockState(base.up(3).east().east(), CROSS_BLOCK.getDefaultState());
     }
 
-    /**
-     * Mengecek apakah blok-blok utama salib masih ada di posisi tersebut.
-     */
     public static boolean exists(ServerWorld world, BlockPos base) {
-        // Cek minimal: tiang tengah bawah, tengah atas, dan satu palang
         return world.getBlockState(base).isOf(CROSS_BLOCK)
-                && world.getBlockState(base.up(2)).isOf(CROSS_BLOCK)
-                && (world.getBlockState(base.up(2).west()).isOf(CROSS_BLOCK)
-                 || world.getBlockState(base.up(2).east()).isOf(CROSS_BLOCK));
+                && world.getBlockState(base.up(3)).isOf(CROSS_BLOCK)
+                && (world.getBlockState(base.up(3).west()).isOf(CROSS_BLOCK)
+                 || world.getBlockState(base.up(3).east()).isOf(CROSS_BLOCK));
     }
 }
