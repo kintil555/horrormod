cb1e84bdadf3349e9e2000dd94cfb302a28b848c
kintil555 <dzikriganteng52637@gmail.com>
2026-03-30 11:55:26 +0700

    Update GameRendererMixin.java

--- src/main/java/com/horrormod/mixin/GameRendererMixin.java
+++ src/main/java/com/horrormod/mixin/GameRendererMixin.java
@@ -7,7 +7,6 @@ import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackValue;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
