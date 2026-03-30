80fc196d9aa03a77f34e0825a442186eff4432f1
kintil555 <dzikriganteng52637@gmail.com>
2026-03-30 11:59:37 +0700

    Extend PauseScreenMixin to inherit from Screen

--- src/main/java/com/horrormod/mixin/PauseScreenMixin.java
+++ src/main/java/com/horrormod/mixin/PauseScreenMixin.java
@@ -2,6 +2,7 @@ package com.horrormod.mixin;
 
 import com.horrormod.client.JumpscareRenderer;
 import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,12 +11,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
-public class PauseScreenMixin {
+public abstract class PauseScreenMixin extends Screen {
+
+    protected PauseScreenMixin(Text title) {
+        super(title);
+    }
 
     @Inject(method = "initWidgets", at = @At("TAIL"))
     private void addJumpscareToggle(CallbackInfo ci) {
-        GameMenuScreen self = (GameMenuScreen)(Object)this;
-
         boolean enabled = JumpscareRenderer.isEnabled();
         String label = enabled ? "\u00a7cDisturbing Images: ON" : "\u00a7aDisturbing Images: OFF";
 
@@ -24,9 +27,9 @@ public class PauseScreenMixin {
             button.setMessage(Text.literal(
                 nowEnabled ? "\u00a7cDisturbing Images: ON" : "\u00a7aDisturbing Images: OFF"));
         })
-        .dimensions(self.width / 2 - 102, self.height / 4 + 120 + 24, 204, 20)
+        .dimensions(this.width / 2 - 102, this.height / 4 + 120 + 24, 204, 20)
         .build();
 
-        self.addDrawableChild(btn);
+        this.addDrawableChild(btn);
     }
 }
